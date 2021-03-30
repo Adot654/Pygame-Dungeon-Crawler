d8517cff87764f6d99549f760f2440f3d0eabbfc
Amal Looby <81536426+Adot654@users.noreply.github.com>
2021-03-30 13:00:57 +0100

    Quick Update
    preparing variables in settings to make collisions and hit boxes for sprites.

--- settings.py
+++ settings.py
@@ -26,9 +26,9 @@ GRIDHEIGHT = HEIGHT / TILESIZE
 
 # PLAYER SETTINGS
 PLAYER_SPEED = 300
-
-# MELEE SETTINGS
-ATTACK_DMG = 15
+PLAYER_ATTACK_DMG = 15
+PLAYER_HEALTH = 100
+PLAYER_HIT_RECT = pg.Rect(0, 0, 0, 0)
 
 # LOADING - PATH for player running frames
 PLAYER_RUN_1_PATH = os.path.join('assets', 'knight_f_run_anim_f0.png')
@@ -42,11 +42,14 @@ PLAYER_IDLE_2_PATH = os.path.join('assets', 'knight_f_idle_anim_f1.png')
 PLAYER_IDLE_3_PATH = os.path.join('assets', 'knight_f_idle_anim_f2.png')
 PLAYER_IDLE_4_PATH = os.path.join('assets', 'knight_f_idle_anim_f3.png') 
 
-# player hit frames
+# PLAYER Damage frames. if player is damaged player this animation.
 PLAYER_HIT_1_PATH = os.path.join('assets', 'knight_f_hit_anim_f0.png')
 
 # MOB SETTINGS
 MOB_SPEED = 100
+MOB_ATTACK_DMG = 10
+MOB_HEALTH = 100
+MOB_HIT_RECT = pg.Rect(0, 0, 0, 0)
 
 # LOADING - PATH for mob running frames
 MOB_RUN_1_PATH = os.path.join('assets', 'goblin_run_anim_f0.png')
